9126cde56e2db25ccbca9b02f53e3dc1e9f5c0f4
sooyun <suy0226@naver.com>
2022-07-11 22:20:53 +0900

    싱글 파일 컴포넌트

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<!-- 컴포넌트 이름은 최소 2단어 이상 -->
+<template>
+  <header>
+      <h1>{{propsdata}}</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function() {
+            this.$emit('renew');
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
