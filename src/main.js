4dba065c6fe57d8c8c58d3aa4aad79d301f17324
sooyun <suy0226@naver.com>
2022-07-11 22:43:00 +0900

    로그인 폼
    - axios 설치

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import AppUser from './AppUser'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(AppUser),
 }).$mount('#app')
